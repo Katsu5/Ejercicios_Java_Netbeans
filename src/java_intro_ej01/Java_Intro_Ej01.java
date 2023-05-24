72b6b93d042019c2a7150b1032469e6aa4e0787f
Katia Susa <susa0686@gmail.com>
2023-05-24 18:48:53 -0500

    Quinto ejercicio

--- src/java_intro_ej01/Java_Intro_Ej01.java
+++ src/java_intro_ej01/Java_Intro_Ej01.java
@@ -8,7 +8,8 @@ public class Java_Intro_Ej01 {
         
        //Java_Intro_Ej02.metodo2();
        //Java_Intro_Ej03.metodo3();
-       Java_Intro_Ej04.metodo4();
+       //Java_Intro_Ej04.metodo4();
+       Java_Intro_Ej05.metodo5();
 
        
         
---
+++ src/java_intro_ej01/Java_Intro_Ej05.java
@@ -0,0 +1,20 @@
+
+package java_intro_ej01;
+import java.util.Scanner;
+
+public class Java_Intro_Ej05 {
+    public static void metodo5() {
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.println("Ingrese 2 numero");
+        int numero1 = leer.nextInt();
+        int numero2 = leer.nextInt();
+        
+        if (numero1 >25 || numero2 > 25 ){
+            System.out.println(" Al menos un numero es mayor que 25");
+        }else{
+            System.out.println("Ningun numero es mayor de 25");
+        }
+             
+    }
+}
